cfe02ff9a77d7482fa59dd37dd257c052201cb6e
hiper2d <hiper2d@gmail.com>
2018-04-29 14:32:01 +0200

    Updated Kotlin to 1.2.41.

--- README.md
+++ README.md
@@ -4,6 +4,5 @@ Java 9 Concurrency Cookbook recipes in Kotlin
 [![TravisCI Build](https://travis-ci.org/hiper2d/java9-concurrency-cookbook.svg)](https://travis-ci.org/hiper2d/java9-concurrency-cookbook)
 
 ### Technology stack
-* Kotlin 1.2.40
-* Gradle 4.7 with Kotlin Script
-* jUnit 5.2
+* Gradle 4.7 with Kotlin Script and jUnit 5
+* Kotlin 1.2.41
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,19 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+plugins {
+    kotlin("jvm") version "1.2.41"
+}
+
 val hamcrestVersion: Any? by project
 val junitJupiterVersion: Any? by project
 val log4jVersion: Any? by project
-
-plugins {
-    kotlin("jvm") version "1.2.40"
-}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jre8")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:$log4jVersion")
 
     testCompileOnly("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
