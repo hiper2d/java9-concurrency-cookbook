bfd5c0abb6f7f49420a99b95b506ecea31100c31
Alexey <hiper2d@gmail.com>
2018-06-08 07:51:31 -0400

    Added a me.champeau.gradle.jmh plugin to Gradle config. Moved jmh test benchmark to a separate source folder.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,15 +4,15 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     base
     kotlin("jvm") version "1.2.41"
+    id("me.champeau.gradle.jmh") version "0.4.6"
 }
 
 val hamcrestVersion: Any? by project
-val jmhVersion: Any? by project
 val junitJupiterVersion: Any? by project
 val kotlinCoroutinesVersion: Any? by project
 val log4jVersion: Any? by project
 
-kotlin { // type is KotlinJvmProjectExtension
+kotlin {
     experimental.coroutines = Coroutines.ENABLE
 }
 
@@ -34,12 +34,11 @@ repositories {
 }
 
 dependencies {
+    jmhImplementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinCoroutinesVersion")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:$log4jVersion")
-
-    implementation("org.openjdk.jmh:jmh-core:$jmhVersion")
-    implementation("org.openjdk.jmh:jmh-generator-annprocess:$jmhVersion")
 
     testImplementation("org.hamcrest:java-hamcrest:$hamcrestVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
