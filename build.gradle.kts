e58e031cd6361437d8f7b2597f841271c070fff1
Alexey <hiper2d@gmail.com>
2018-09-02 09:22:25 -0400

    Updated Kotlin to 1.2.61

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     base
-    kotlin("jvm") version "1.2.41"
+    kotlin("jvm") version "1.2.61"
     id("me.champeau.gradle.jmh") version "0.4.6"
 }
 
